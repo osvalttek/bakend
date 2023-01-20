9e825eb385019bcc824a7ef7b507654dffafb1bb
Osvaldito <osvaldo.devmate@gmail.com>
2023-01-20 19:39:36 -0300

    parte dos de la aplicacion de websocket

--- server.js
+++ server.js
@@ -2,36 +2,24 @@ import express from "express"
 import { createServer } from "http"
 import { Server } from "socket.io"
 
-// esto lo tengo que hacer porque estoy trabajando con type: modelule
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-
 const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer)
-
+const ip = "192.168.0.40"
 
 app.use(express.static("public"))
-
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
+    res.sendFile(`index.html`)
 })
 
-
+let messages = []
 io.on("connection", (socket) => {
     console.log("hola", socket.id)
-    socket.emit("pepe", "hola como estas? soy el servidor")
-    socket.on("respuesta", data => {
-        console.log("🚀", data)
+    socket.emit("messages", messages)
+    socket.on("newMessage", data => {
+        messages.push(data)
+        io.sockets.emit("messages", messages)
     })
-
-
 })
 
-
-
-
-httpServer.listen(3000, () => console.log("servidor ok"))
+httpServer.listen(8080, () => console.log("servidor ok"))
